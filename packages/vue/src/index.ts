4131894be025314f5b32b0b4c6d5c2c5e49fbedd
konglingzhan_zt <1397424822@qq.com>
2020-12-11 17:39:09 +0800

    add prettierignore and eslintignore

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,8 +1,10 @@
 import { initDev } from './dev';
-interface Person {
-  name: string;
-  age: number;
-}
+// interface Person {
+//   name: string;
+//   age: number;
+// }
 console.log('dd=', __DD);
 console.log('jjj=', __HH);
 initDev();
+
+export * from '@vue/compiler-core';
--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,32 @@
-import commonjs from 'rollup-plugin-commonjs';//这两个插件可以让你加载Node.js里面的CommonJS模块
-import node from 'rollup-plugin-node-resolve';//这两个插件可以让你加载Node.js里面的CommonJS模块
+import commonjs from 'rollup-plugin-commonjs'; // 这两个插件可以让你加载Node.js里面的CommonJS模块
+import node from 'rollup-plugin-node-resolve'; // 这两个插件可以让你加载Node.js里面的CommonJS模块
 import typescript from '@rollup/plugin-typescript';
-import ts from 'rollup-plugin-typescript2'
-import replace from '@rollup/plugin-replace'
-import path from 'path'
-const extensions = [
-  '.js',
-  '.ts',
-  '.tsx'
-]
+import ts from 'rollup-plugin-typescript2';
+import replace from '@rollup/plugin-replace'; // set the global variable to ts
+import path from 'path';
+
+const extensions = ['.js', '.ts', '.tsx'];
 // ts
 const tsPlugin = ts({
-  tsconfig: path.resolve(__dirname,'./tsconfig.json'), // 导入本地ts配置
-  extensions
-})
-
+  tsconfig: path.resolve(__dirname, './tsconfig.json'), // 导入本地ts配置
+  extensions,
+});
 
 export default {
-  input:"./packages/vue/src/index.ts",
-  output:{
-    file:"dist/bund.js",
-    format:"iife",
-    name:'vue'
+  input: './packages/vue/src/index.ts',
+  output: {
+    name: 'vue',
+
+    file: 'dist/bund.js',
+    format: 'iife',
   },
-  plugins:[
+  plugins: [
     // node(),
     // commonjs(),
     tsPlugin,
     replace({
-      '__DD': JSON.stringify(process.env),
-      '__HH': true,
-    })
-  ]
-}
+      __DD: JSON.stringify(process.env),
+      __HH: true,
+    }),
+  ],
+};
